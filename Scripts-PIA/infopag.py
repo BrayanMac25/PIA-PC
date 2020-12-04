4e9277a5200e6b299969cc985e1c4c9754ed176a
BrayanMac25 <73508043+BrayanMac25@users.noreply.github.com>
2020-12-04 16:40:20 -0600

    Script

---
+++ Scripts-PIA/infopag.py
@@ -0,0 +1,7 @@
+import socket
+def info(x):
+	ipres = socket.gethostbyname_ex(x)
+	ser = socket.getfqdn(x)
+	print("Ip donde responde la peticion", ipres[2])
+	print("Nombre del servidor donde responde", ser)
+info('google.com')
